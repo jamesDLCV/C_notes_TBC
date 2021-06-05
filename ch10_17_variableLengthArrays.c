6bada7a2085d335402052b1f285e3923e3164303
jamesDLCV <69728729+jamesDLCV@users.noreply.github.com>
2021-06-05 14:53:12 +0800

    Add files via upload

---
+++ ch10_17_variableLengthArrays.c
@@ -0,0 +1,24 @@
+// Variable-Length Arrays (VALs)
+// 변수로 길이를 정할 수 있는 배열
+// 초기화할 때, 컴파일 타임에 알려져 있는 상수만으로 길이를 정할 수 있는 
+// 것이 아니라, scanf로 변수의 길이를 입력 받아서 
+// 길이를 정할 수 있는 배열에 대해서 살펴 보자. 
+// 가변 길이 배열, 가변 크기 배열
+// 배열의 크기를 아무 때나 바꿀 수 있는 것은 아니다. 
+// VALs는 visual studio에서 컴파일이 안 된다. 
+
+#include <stdio.h>
+
+int main() {
+
+    /*
+    * 안타깝게도 visual studio에서는 컴파일이 안 된다. 
+    int n = 5;
+    float my_arr[n];
+    */
+
+
+
+
+    return 0;
+}
